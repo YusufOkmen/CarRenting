58478f58f70c58f9cd23bbc43c87b9730772a436
hısaeeerq@gmail.com <yusufkmen@hotmail.com>
2024-12-07 14:55:59 +0300

    the class 'Araclar' has been created

---
+++ main.py
@@ -0,0 +1,10 @@
+tumAraclar = []
+
+
+class Araclar:
+    def __init__(self, aracMarka, aracModel, gunlukFiyat, kiraDurum, tahminiKira):
+        self.marka = aracMarka
+        self.model = aracModel
+        self.fiyat = gunlukFiyat
+        self.kira = kiraDurum
+        self.tahmin = tahminiKira
